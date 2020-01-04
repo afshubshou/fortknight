9f801e3927b420a1c1592350a35faaa321c1799b
ahmetfs77 <ahmetf.soygur@gmail.com>
2020-01-04 22:56:47 +0300

    Updated-1

---
+++ Fortknight/Assets/Scripts/ShopProcess.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShopProcess : MonoBehaviour
+{
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        
+    }
+
+
+
+    void Start()
+    {
+        
+    }
+
+
+    void OnClick()
+    {
+        var random = Random.Range(0, 100);
+
+
+
+        if (random >= 0 && random <= 39.7)
+        {
+            
+
+        }
+
+        else if (random>39.7 && random <=59.6)
+        {
+
+        }
+        else if (random > 59.6 && random <= 72.8)
+        {
+
+        }
+        else if (random > 72.9 && random <= 86)
+        {
+
+        }
+        else if (random > 86 && random <= 96.6)
+        {
+
+        }
+        else if (random > 96.6 && random <= 100)
+        {
+
+        }
+
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
+    
+ 
